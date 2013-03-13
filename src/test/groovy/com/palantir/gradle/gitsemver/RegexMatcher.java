d57fcc9cac48caeb0238ca60bbca74eb19848613
Eric Anderson <eanderson@palantir.com>
2013-03-13 12:46:55 -0400

    Makes eclipse happy

--- src/test/groovy/com/palantir/gradle/gitsemver/RegexMatcher.java
+++ src/test/groovy/com/palantir/gradle/gitsemver/RegexMatcher.java
@@ -3,20 +3,18 @@ package com.palantir.gradle.gitsemver;
 import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
 
-public class RegexMatcher extends BaseMatcher<String>{
+public class RegexMatcher extends BaseMatcher<String> {
     private final String regex;
 
     public RegexMatcher(String regex){
         this.regex = regex;
     }
 
-    @Override
     public boolean matches(Object o){
         return ((String)o).matches(regex);
 
     }
 
-    @Override
     public void describeTo(Description description){
         description.appendText("matches regex=");
     }
